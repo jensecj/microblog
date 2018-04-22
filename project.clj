3d36dead8f3a06888e38f6de257f706596763a4f
Jens Christian Jensen <jensecj@gmail.com>
2018-04-22 17:35:30 +0200

    add database accessor and postgresql driver as dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url ""
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"],
+                 [org.clojure/java.jdbc "0.7.5"],
+                 [org.postgresql/postgresql "42.2.2"]]
   :main ^:skip-aot microblog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
