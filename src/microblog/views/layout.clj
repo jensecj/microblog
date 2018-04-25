cd41e96eab061f547e647be0925d3baeb2f37bc2
Jens Christian Jensen <jensecj@gmail.com>
2018-04-25 08:54:57 +0200

    added common layout to view

--- src/microblog/views/layout.clj
+++ src/microblog/views/layout.clj
@@ -1,1 +1,28 @@
-(ns microblog.views.layout)
+(ns microblog.views.layout
+  (:require
+   [hiccup.page :refer [html5 include-css]]))
+
+(defn common [title & body]
+  (html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+    [:title title]
+    (include-css
+     "/css/reset.css"
+     "/css/base.css"
+     "/css/skeleton.css"
+     "/css/screen.css")]
+   [:body
+    [:div {:id "header"}
+     [:h1 {:class "container"} "microblog"]]
+    [:div {:id "content" :class "container"} body]
+    [:div {:id "footer"}]]))
+
+(defn simple [content]
+  (common "microblog web app" content))
+
+(defn four-oh-four []
+  (common "Page Not Found"
+          [:div {:id "four-oh-four"}
+           "The page you requested could not be found"]))
