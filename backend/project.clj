e3e0aca1db036748aefe2ff12ae0ebd9a6db89c0
Jens Christian Jensen <jensecj@gmail.com>
2018-05-01 09:25:02 +0200

    update project.clj, start using migratus-lein, for easy migrations

--- backend/project.clj
+++ backend/project.clj
@@ -3,6 +3,7 @@
   :url ""
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/java.jdbc "0.7.5"]
                  [environ "1.1.0"]
@@ -18,14 +19,20 @@
                  [ring/ring-mock "0.3.2"]
                  [ring-cors "0.1.12"]
                  [cheshire "5.8.0"]
-                 [buddy "2.0.0"]
-                 ]
-  :plugins [[lein-ring "0.12.4"]
-            [lein-environ "1.1.0"]]
-  :ring {:handler backend.server/app
-         :auto-reload? true
-         :auto-refresh? true
-         }
+                 [buddy "2.0.0"]]
+
+  :plugins [[lein-environ "1.1.0"]
+            [migratus-lein "0.5.7"]]
+
+  :migratus {:store :database
+             :migration-dir "migrations/"
+             :db ~(format "postgres://%s:%s/%s?user=%s&password=%s"
+                          (System/getenv "MICROBLOG_DATABASE_URL")
+                          (System/getenv "MICROBLOG_DATABASE_PORT")
+                          (System/getenv "MICROBLOG_DATABASE_NAME")
+                          (System/getenv "MICROBLOG_DATABASE_USER")
+                          (System/getenv "MICROBLOG_DATABASE_PASSWORD"))}
+
   :main ^:skip-aot backend.server
   :jvm-opts ["-Xmx1g"]
   :target-path "target/%s"
