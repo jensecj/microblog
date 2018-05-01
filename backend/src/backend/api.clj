eafc20cd25bc8ce233a3b42ac5db6dcd2504ed6e
Jens Christian Jensen <jensecj@gmail.com>
2018-05-01 09:29:00 +0200

    move api-handling into api.clj. add cookie session, and wrap.

---
+++ backend/src/backend/api.clj
@@ -0,0 +1,109 @@
+(ns backend.api
+  (:require [environ.core :refer [env]]
+            [compojure.api.sweet :refer :all]
+            [backend.db :refer [Database Post User]]
+            [backend.dbprotocol :as db]
+            [ring.util.http-response :refer :all]
+            [compojure.api.meta :refer [restructure-param]]
+            [ring.middleware.cors :refer [wrap-cors]]
+            [taoensso.timbre :as log]
+            [buddy.auth :as ba]
+            [buddy.auth.backends.session :as session]
+            [buddy.auth.middleware :as bam]
+            [buddy.hashers :as bh]
+            [buddy.auth.accessrules :as baa]
+            ))
+
+;; API
+(defn get-user-by-name [db username]
+  (db/get-user-by-name db username))
+
+(defn get-all-posts [db]
+  (db/get-all-posts db))
+
+(defn authenticate-user [db username password]
+  (let [user (get-user-by-name db username)]
+    (bh/check password (:hash user))))
+
+;; AUTH
+(defn access-error [request value]
+  (unauthorized value))
+
+(defn wrap-rule [handler rule]
+  (-> handler
+      (baa/wrap-access-rules
+       {:rules [{:pattern #".*" :handler rule}]
+        :on-error access-error})))
+
+(defmethod restructure-param :auth-rules
+  [_ rule acc]
+  (update-in acc [:middleware] conj [wrap-rule rule]))
+
+(defn authenticated? [req]
+  (ba/authenticated? req))
+
+;; HANDLER
+
+(defn fake-user []
+  {:username "jens" :hash "bcrypt+sha512$39566145319119818b08070497a5e161$12$ba92e3006090e8e3bc1659599a90136a266773527cd92726"})
+(defn- api-routes [db]
+  (context "/api" []
+    :tags ["api"]
+
+    (POST "/login" []
+      :summary "logs a user in"
+      :body-params [username :- s/Str, password :- s/Str]
+      (let [user (fake-user)]
+        (log/info "some tried to login! (" username "," password ")")
+
+        (if (authenticate-user db username password)
+          (do
+            (log/info "login success!")
+            (assoc-in (ok) [:session :identity] {:username (:username user)}))
+          (do
+            (log/info "login failed!")
+            (assoc-in (forbidden) [:session :identity] nil)))
+        )
+      )
+
+    (GET "/get-user-by-name" []
+      :summary "Get a users record by username"
+      :return {:result User}
+      :query-params [username :- s/Str]
+      (ok {:result (get-user-by-name db username)}))
+
+    (POST "/new-post" []
+      :summary "Add a new post"
+      :body-params [post :- s/Str]
+      (db/add-post db post))
+    (GET "/all-posts" []
+      :summary "Get all available posts"
+      :return {:result [Post]}
+      :auth-rules authenticated?
+      (ok {:result (get-all-posts db)}))
+    (GET "/posts-by-offset" []
+      :summary "Get n posts, starting at offset"
+      :query-params [num_posts :- s/Int, offset :- s/Int]
+      :return {:result [Post]}
+      (ok {:result (db/get-posts-by-offset db num_posts offset)}))
+
+    ))
+
+(defn- app [db]
+  (api
+   {:swagger
+    {:ui "/api-docs"
+     :spec "/swagger.json"
+     :data {:info {:title "Simple REST API"
+                   :description "simple rest api example"}
+            :tags [{:name "api", :description "some api endpoints"}]
+            :consumes ["application/json"]
+            :produces ["application/json"]}}}
+   (api-routes db)
+   (ANY "*" [] (not-found {:message "invalid request"}))))
+
+(defn handler [db]
+  (-> (app db)
+      (wrap-cors :access-control-allow-origin [#"http://localhost:8080" #"http://localhost:3449"]
+                 :access-control-allow-methods [:get :post]))
+  )
--- backend/src/backend/server.clj
+++ backend/src/backend/server.clj
@@ -1,80 +1,41 @@
 (ns backend.server
   (:gen-class)
   (:require [environ.core :refer [env]]
-            [compojure.api.sweet :refer :all]
-            [backend.db :refer [Database Post]]
+            [backend.db :refer [Database Post User]]
             [backend.dbprotocol :as db]
             [mount.core :as mount :refer [defstate]]
-            [org.httpkit.server :as serv]
-            [ring.util.http-response :refer :all]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [schema.core :as s]
             [taoensso.timbre :as log]
+            [backend.api :as api]
+            [ring.middleware.session :as rms]
+            [ring.util.http-response :as response]
+            [compojure.api.meta :refer [restructure-param]]
+            [buddy.auth :as ba]
+            [buddy.auth.accessrules :as baa]
+            [buddy.auth.backends.session :as session]
+            [buddy.auth.middleware :as bam]
             [buddy.hashers :as bh]
             ))
 
-(defn- get-user-by-name [username]
-  {:username "jens" :hash (bh/derive "123")})
-
-(defn- api-routes [db]
-  (context "/api" []
-           :tags ["api"]
-
-           (POST "/login" []
-                 :summary "logs a user in"
-                 :body-params [username :- s/Str, hash :- s/Str]
-                 (let [user (get-user-by-name username)]
-                   (if (bh/check hash (:hash user))
-                     (assoc-in (ok) [:session :identity] {:username (:username user)})
-                     (assoc-in (forbidden) [:session :identity] nil)))
-                 )
-
-           (GET "/all-posts" []
-                :summary "Get all available posts"
-                :return {:result [Post]}
-                (ok {:result (db/get-all-posts db)}))
-           (GET "/posts-by-offset" []
-                :summary "Get n posts, starting at offset"
-                :query-params [num_posts :- s/Int, offset :- s/Int]
-                :return {:result [Post]}
-                (ok {:result (db/get-posts-by-offset db num_posts offset)}))
-           (POST "/new-post" []
-                 :summary "Add a new post"
-                 :body-params [post :- s/Str]
-                 (db/add-post db post))
-
-           (GET "/page/:p" []
-                :path-params [p :- s/Int]
-                :return {:result [Post]}
-                (ok {:result (db/get-posts-by-offset db 3 p)}))
-           ))
-
-(defn- app [db]
-  (api
-   {:swagger
-    {:ui "/api-docs"
-     :spec "/swagger.json"
-     :data {:info {:title "Simple REST API"
-                   :description "simple rest api example"}
-            :tags [{:name "api", :description "some api endpoints"}]
-            :consumes ["application/json"]
-            :produces ["application/json"]}}}
-   (api-routes db)
-   (ANY "*" [] (not-found {:message "invalid request"}))))
-
-(defn handler [db]
-  (wrap-cors (app db)
-             :access-control-allow-origin [#"http://localhost:3449"]
-             :access-control-allow-methods [:get :post]))
-
 (def config {:host (env :microblog-api-url)
              :port (read-string (env :microblog-api-port))})
+
+(def cookie-name "microblog-cookie")
+(def auth-backend (session/session-backend))
+
+(defn wrap-app-session [handler]
+  (-> handler
+      (bam/wrap-authorization auth-backend)
+      (bam/wrap-authentication auth-backend)
+      (rms/wrap-session {:cookie cookie-name})))
+
+(defn- handle [handler]
+  (-> (wrap-app-session handler)))
 
 (defstate Server
   :start (do
            (log/info "starting server component")
            (log/info (format "backend is running: %s" config))
-           (serv/run-server (handler Database) config))
+           (serv/run-server (handle (api/handler Database)) config))
   :stop (do
           (log/info "stopping server component")
           (Server :timeout 100)))
